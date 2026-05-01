fb5def5f1ff93c603b44742f373484f884e1bfd4
Nguyễn Việt Dũng <dungnasan126@gmail.com>
2026-05-01 09:55:08 +0700

    feat: initialize spring boot project

---
+++ src/main/java/com/dung/pump_recommendation/PumpRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.dung.pump_recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumpRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dung/pump_recommendation/PumpRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dung.pump_recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
